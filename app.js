1ebf6278912cb07af9e580822e6316e24876c5c8
sunnydan <danielhilla@gmail.com>
2017-10-02 15:46:39 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// get the http module:
+var http = require('http');
+// fs module allows us to read and write content for responses!!
+var fs = require('fs');
+// creating a server using http module:
+var server = http.createServer(function (request, response){
+    // see what URL the clients are requesting:
+    console.log('client request URL: ', request.url);
+    // this is how we do routing:
+    if(request.url === '/') {
+        fs.readFile('index.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    else if (request.url === "/ninjas") {
+         fs.readFile('ninjas.html', 'utf8', function (errors, contents){
+             response.writeHead(200, {'Content-type': 'text/html'});
+             response.write(contents); 
+             response.end();
+         });
+    }
+    else if (request.url === "/dojos/new") {
+        fs.readFile('dojos.html', 'utf8', function (errors, contents){
+            response.writeHead(200, {'Content-type': 'text/html'});
+            response.write(contents); 
+            response.end();
+        });
+    }
+    // request didn't match anything:
+    else {
+        response.end('404: Page Not Found');
+    }
+});
+// tell your server which port to run on
+var port = 8000;
+server.listen(port);
+// print to terminal window
+console.log("Running in localhost at port " + port.toString());
